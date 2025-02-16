16f0e05b7c109922ee978e03668bf374c3a8c987
morteza <morteza.abolfathi73@gmail.com>
2025-02-16 01:24:09 -0800

    config liara server

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-};
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  distDir: ".next",
+}
 
-export default nextConfig;
+export default nextConfig
